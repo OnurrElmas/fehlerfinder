850ec7a565d18b37c4ebe476abb2438492f0b83f
Onur Elmas <57056680+OnurrElmas@users.noreply.github.com>
2022-06-07 16:32:51 +0200

    Update fehlerfinder.py

--- fehlerfinder.py
+++ fehlerfinder.py
@@ -18,28 +18,32 @@ def find_fehlers(logfile_content):
     finded_EGS = 0
     finded_DME = 0
     finded_INV = 0
+    finded_EPK_Pr = 0
     for match in re.finditer(r"does not match",logfile_content):
         if re.findall(r"CCU", logfile_content[match.start()-100:match.start()]):
             if finded_CCU == 0:
                 fehlers.append("CCU A2L Version stimmt nicht überein ")
                 finded_CCU = 1
-        if re.findall(r"_default_ModuleName_BMU version", logfile_content[match.start()-100:match.start()]):
+        if re.findall(r"BMU", logfile_content[match.start()-100:match.start()]):
             if finded_BMU == 0:
                 fehlers.append("BMU A2L Version stimmt nicht überein ")
                 finded_BMU = 1
-        if re.findall(r"CPP ", logfile_content[match.start()-100:match.start()]):
+        if re.findall(r"CPP ", logfile_content[match.start()-100:match.start()]) | re.findall(r"EGS", logfile_content[match.start()-100:match.start()]):
             if finded_EGS == 0:
                 fehlers.append("EGS A2L Version stimmt nicht überein ")
                 finded_EGS = 1
-        if re.findall(r"_default_ModuleName ", logfile_content[match.start()-100:match.start()]):
+        if re.findall(r"_default_ModuleName ", logfile_content[match.start()-100:match.start()])| re.findall(r"DME", logfile_content[match.start()-100:match.start()]):
             if finded_DME == 0:
                 fehlers.append("DME A2L Version stimmt nicht überein ")
                 finded_DME = 1
-        if re.findall(r"CPP_INV", logfile_content[match.start()-100:match.start()]):
+        if re.findall(r"INV", logfile_content[match.start()-100:match.start()]):
             if finded_INV == 0:
                 fehlers.append("INV A2L Version stimmt nicht überein ")
                 finded_INV = 1
-        
+    for match in re.finditer(r"INV returned error code 0x24",logfile_content):
+        if finded_EPK_Pr == 0:
+            fehlers.append("INV EPK Prüfung muss deaktiviert werden")
+            finded_EPK_Pr = 1
     return fehlers    
 
                 
